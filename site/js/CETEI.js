dc19ee3c9873e98445f1f4066531ed49d4f356ab
Brandon Taylor <brandontaylor@Brandons-MacBook-Air.local>
2025-05-21 01:38:44 -0500

    Restore full CETEI interface with addBehaviors and convert methods

--- site/js/CETEI.js
+++ site/js/CETEI.js
@@ -1,4 +1,15 @@
-function CETEI() {}
+function CETEI() {
+  this.behaviors = {};
+  this.extensions = {};
+  this.style = null;
+}
+
+CETEI.prototype.addBehaviors = function(styleContent) {
+  const style = document.createElement("style");
+  style.innerHTML = styleContent;
+  document.head.appendChild(style);
+};
+
 CETEI.prototype.getHTML = function(file, callback) {
   fetch(file)
     .then(res => res.text())
@@ -15,6 +26,29 @@ CETEI.prototype.getHTML = function(file, callback) {
     .catch(err => {
       console.error("Failed to load or parse TEI XML:", err);
     });
+};
+
+CETEI.prototype.convert = function(xml) {
+  const transformed = document.createElement("div");
+  const root = xml.documentElement;
+  const walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT, null, false);
+
+  let node;
+  while ((node = walker.nextNode())) {
+    if (node.nodeType === Node.ELEMENT_NODE) {
+      const htmlElement = document.createElement(`tei-${node.localName}`);
+      for (let attr of node.attributes) {
+        htmlElement.setAttribute(attr.name, attr.value);
+      }
+      if (node.firstChild) {
+        htmlElement.appendChild(node.firstChild);
+      }
+      node.parentNode.replaceChild(htmlElement, node);
+    }
+  }
+
+  transformed.appendChild(root);
+  return transformed;
 };
 
 window.CETEI = CETEI;
