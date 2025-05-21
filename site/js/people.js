2cf4c17af3898f7ab52f568a0f192044260d20da
Brandon Taylor <brandontaylor@Brandons-MacBook-Air.local>
2025-05-21 11:44:52 -0500

    Add interactive Personography page and people.js dataset

---
+++ site/js/people.js
@@ -0,0 +1,51 @@
+const people = [
+  {
+    "id": "carleton-dudley",
+    "name": "Dudley Carleton",
+    "shortBio": "English diplomat and key correspondent of John Chamberlain. Later became Viscount Dorchester.",
+    "letters": [
+      1,
+      2,
+      3,
+      4,
+      5
+    ],
+    "externalLinks": {
+      "wikipedia": "https://en.wikipedia.org/wiki/Dudley_Carleton,_1st_Viscount_Dorchester",
+      "odnb": "https://www.oxforddnb.com/display/10.1093/ref:odnb/9780198614128.001.0001/odnb-9780198614128-e-4713",
+      "britannica": ""
+    }
+  },
+  {
+    "id": "chamberlain-john",
+    "name": "John Chamberlain",
+    "shortBio": "English letter writer and political observer. Central figure of this collection.",
+    "letters": [
+      1,
+      2,
+      3,
+      4,
+      5
+    ],
+    "externalLinks": {
+      "wikipedia": "https://en.wikipedia.org/wiki/John_Chamberlain_(letter_writer)",
+      "odnb": "",
+      "britannica": ""
+    }
+  },
+  {
+    "id": "mathew-tobie",
+    "name": "Tobie Mathew",
+    "shortBio": "Later Archbishop of York; associated with political-religious tensions of the late Elizabethan era.",
+    "letters": [
+      1
+    ],
+    "externalLinks": {
+      "wikipedia": "https://en.wikipedia.org/wiki/Tobias_Matthew",
+      "odnb": "",
+      "britannica": ""
+    }
+  }
+];
+
+export default people;
